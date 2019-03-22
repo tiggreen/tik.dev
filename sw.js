2baa522d0618e62d29f31427a9f0163b113857b9
Tigran Hakobyan <tigran@bufferapp.com>
2019-03-22 15:18:56 -0400

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-38b30a90a33ef0b477c7.js"
+    "url": "webpack-runtime-7cf2f0392fb68b440c90.js"
   },
   {
     "url": "app.122e3b7e148ea91fc31f.css"
@@ -39,7 +39,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "5757bb769582cbd757e0b73aa525aee6"
+    "revision": "2092bfa2d8ec5e1262ad329de208b262"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
