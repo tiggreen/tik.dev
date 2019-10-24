8568c9684fa7211b3da3eb969f1a22180610faa6
Tigran Hakobyan <tigran@bufferapp.com>
2019-10-24 07:53:09 -0400

    Updates

--- sw.js
+++ sw.js
@@ -26,20 +26,20 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-69df831fb86a7d637baf.js"
+    "url": "webpack-runtime-006f3097e6776228dac4.js"
   },
   {
     "url": "app.122e3b7e148ea91fc31f.css"
   },
   {
-    "url": "app-0443cf275a06a11d0ed6.js"
+    "url": "app-a3c47f0b87643f3ab445.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-7e1e5d87c87c3dd4815e.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "cb547868e874111b542f30ff4f592a1b"
+    "revision": "2df7e451e540278fb0ab8252ce367e0b"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
