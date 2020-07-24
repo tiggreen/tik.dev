7945153fa9a3da1672fc299ace96f14a696e9dd1
Tigran Hakobyan <tigran@bufferapp.com>
2020-07-23 20:27:25 -0400

    Add armenian essay section

--- config.js
+++ config.js
@@ -30,6 +30,10 @@ module.exports = {
       label: 'People',
       path: '/people',
     },
+    {
+      label: 'Հայերեն մտքեր',
+      path: '/essays-in-armenian',
+    },
   ],
   author: {
     name: 'Tigran Hakobyan',
